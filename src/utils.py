08af86f6dc9f31cc6cf6965c7268ac208f0410ab
Jo <joyce.sebeya@aivancity.education>
2025-10-23 11:13:33 +0200

    Add multiply and ddivide functions

--- src/utils.py
+++ src/utils.py
@@ -13,3 +13,18 @@ def subtract(*args):
         result -= num
     return result
 
+def multiply(*args):
+    result = 1
+    for num in args:
+        result *= num
+    return result
+
+def divide(*args):
+    if not args:
+        return None
+    result = args[0]
+    for num in args[1:]:
+        if num == 0:
+            raise ValueError("Cannot divide by zero")
+        result /= num
+    return result
